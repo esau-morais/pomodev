90f59e200ed9f9a922781956c7f290be0c9662fd
esau-morais <esaumorais7@gmail.com>
2023-03-18 22:56:51 -0300

    fix: add validation

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ const startTimer = async (
   rounds: number
 ) => {
   for (let i = 0; i < rounds; i++) {
-    // Focus period
     console.log(`Starting focus period (${i + 1}/${rounds})...`)
     await runPeriod(
       focusPeriod.duration,
@@ -17,7 +16,6 @@ const startTimer = async (
     )
     console.log('Focus period ended.')
 
-    // Break period
     console.log(`Starting break period (${i + 1}/${rounds})...`)
     await runPeriod(
       breakPeriod.duration,
@@ -51,15 +49,17 @@ const runPeriod = async (duration: number, periodName: string) => {
   process.stdout.write('\n')
 }
 
+const durations: Duration[] = [
+  { label: '1 minutes', value: 1 },
+  { label: '10 minutes', value: 10 },
+  { label: '15 minutes', value: 15 },
+  { label: '20 minutes', value: 20 },
+  { label: '25 minutes', value: 25 },
+  { label: '30 minutes', value: 30 },
+]
+
 const main = async () => {
-  const durations: Duration[] = [
-    { label: '5 minutes', value: 5 },
-    { label: '10 minutes', value: 10 },
-    { label: '15 minutes', value: 15 },
-    { label: '20 minutes', value: 20 },
-    { label: '25 minutes', value: 25 },
-    { label: '30 minutes', value: 30 },
-  ]
+  prompts.intro('⏳ Welcome to pomocli!')
 
   const focusDuration = (await prompts.select({
     message: 'Select focus duration:',
@@ -74,9 +74,15 @@ const main = async () => {
   const rounds = await prompts.text({
     message: 'How many rounds do you want to do?',
     initialValue: '4',
+    validate: (value) =>
+      !value
+        ? 'Value is required'
+        : isNaN(parseInt(value)) || parseInt(value) <= 0
+          ? 'Value must be a positive number'
+          : undefined,
   })
 
-  prompts.intro('⏳ Welcome to pomocli!')
+  console.clear()
 
   await startTimer(
     { name: 'Focus', duration: focusDuration * 60 },
@@ -87,4 +93,4 @@ const main = async () => {
   prompts.outro("🎉 You've got it!")
 }
 
-main()
+main().catch(console.error)
